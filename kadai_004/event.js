8c3c8e89f8d9ea72ac9267b580107c91a2a31693
J-akayama <ta.200206@gmail.com>
2023-10-06 23:36:51 +0900

    commit

---
+++ kadai_004/event.js
@@ -0,0 +1,10 @@
+// HTMLドキュメントが読み込み完了した際に、「loadイベントが発生しました」とコンソールに出力
+  $(window).on('load', function(){
+    console.log('loadイベントが発生しました');
+  });
+
+
+// 画面スクロールした際に、「scrollイベントが発生しました」とコンソールに出力
+  $(window).on('scroll', function(){
+    console.log('scrollイベントが発生しました');
+  });
